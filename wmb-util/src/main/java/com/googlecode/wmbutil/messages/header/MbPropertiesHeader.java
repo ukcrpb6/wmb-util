0fc99006c2c02cfa691e0259f5a10dfd0f55b191
Bob Browning <rpb6@drache.co.uk>
2012-10-12 11:34:24 +0100

    Add properties header

--- wmb-util/src/main/java/com/googlecode/wmbutil/messages/header/MbPropertiesHeader.java
+++ wmb-util/src/main/java/com/googlecode/wmbutil/messages/header/MbPropertiesHeader.java
@@ -2,29 +2,151 @@ package com.googlecode.wmbutil.messages.header;
 
 import com.ibm.broker.plugin.MbException;
 
+import java.util.Date;
+
 /**
  * @author Bob Browning <bob.browning@pressassociation.com>
  */
 public interface MbPropertiesHeader extends MbHeader {
 
-    public static final String MSG_SET = "MessageSet";
-    public static final String MSG_TYPE = "MessageType";
-    public static final String FORMAT = "MessageFormat";
-    public static final String TOPIC = "Topic";
+    @Deprecated public static final String MSG_SET = "MessageSet";
+
+    @Deprecated public static final String MSG_TYPE = "MessageType";
+
+    @Deprecated public static final String FORMAT = "MessageFormat";
+
+    @Deprecated public static final String TOPIC = "Topic";
+
+    public static final String CodedCharSetId = "CodedCharSetId";
+
+    public static final String CreationTime = "CreationTime";
+
+    public static final String ContentType = "ContentType";
+
+    public static final String Encoding = "Encoding";
+
+    public static final String ExpirationTime = "ExpirationTime";
+
+    public static final String IdentityMappedIssuedBy = "IdentityMappedIssuedBy";
+
+    public static final String IdentityMappedPassword = "IdentityMappedPassword";
+
+    public static final String IdentityMappedToken = "IdentityMappedToken";
+
+    public static final String IdentityMappedType = "IdentityMappedType";
+
+    public static final String IdentitySourceIssuedBy = "IdentitySourceIssuedBy";
+
+    public static final String IdentitySourcePassword = "IdentitySourcePassword";
+
+    public static final String IdentitySourceToken = "IdentitySourceToken";
+
+    public static final String IdentitySourceType = "IdentitySourceType";
+
+    public static final String MessageFormat = "MessageFormat";
+
+    public static final String MessageSet = "MessageSet";
+
+    public static final String MessageType = "MessageType";
+
+    public static final String Persistence = "Persistence";
+
+    public static final String Priority = "Priority";
+
+    public static final String ReplyIdentifier = "ReplyIdentifier";
+
+    public static final String ReplyProtocol = "ReplyProtocol";
+
+    public static final String Topic = "Topic";
+
+    public static final String Transactional = "Transactional";
+
+    int getCodedCharSetId() throws MbException;
+
+    Date getCreationTime() throws MbException;
+
+    String getContentType() throws MbException;
+
+    int getEncoding() throws MbException;
+
+    Date getExpirationTime() throws MbException;
+
+    String getIdentityMappedIssuedBy() throws MbException;
+
+    String getIdentityMappedPassword() throws MbException;
+
+    String getIdentityMappedToken() throws MbException;
+
+    String getIdentityMappedType() throws MbException;
+
+    String getIdentitySourceIssuedBy() throws MbException;
+
+    String getIdentitySourcePassword() throws MbException;
+
+    String getIdentitySourceToken() throws MbException;
+
+    String getIdentitySourceType() throws MbException;
 
     String getMessageFormat() throws MbException;
 
-    void setMessageFormat(String messageFormat) throws MbException;
-
     String getMessageSet() throws MbException;
-
-    void setMessageSet(String messageSet) throws MbException;
 
     String getMessageType() throws MbException;
 
-    void setMessageType(String messageType) throws MbException;
+    boolean getPersistence() throws MbException;
+
+    int getPriority() throws MbException;
+
+    byte[] getReplyIdentifier() throws MbException;
+
+    String getReplyProtocol() throws MbException;
 
     String getTopic() throws MbException;
 
-    void setTopic(String topic) throws MbException;
+    boolean getTransactional() throws MbException;
+
+    void setCodedCharSetId(int value) throws MbException;
+
+    void setCreationTime(Date value) throws MbException;
+
+    void setContentType(String value) throws MbException;
+
+    void setEncoding(int value) throws MbException;
+
+    void setExpirationTime(Date value) throws MbException;
+
+    void setIdentityMappedIssuedBy(String value) throws MbException;
+
+    void setIdentityMappedPassword(String value) throws MbException;
+
+    void setIdentityMappedToken(String value) throws MbException;
+
+    void setIdentityMappedType(String value) throws MbException;
+
+    void setIdentitySourceIssuedBy(String value) throws MbException;
+
+    void setIdentitySourcePassword(String value) throws MbException;
+
+    void setIdentitySourceToken(String value) throws MbException;
+
+    void setIdentitySourceType(String value) throws MbException;
+
+    void setMessageFormat(String value) throws MbException;
+
+    void setMessageSet(String value) throws MbException;
+
+    void setMessageType(String value) throws MbException;
+
+    void setPersistence(boolean value) throws MbException;
+
+    void setPriority(int value) throws MbException;
+
+    void setReplyIdentifier(byte[] value) throws MbException;
+
+    void setReplyProtocol(String value) throws MbException;
+
+    void setTopic(String value) throws MbException;
+
+    void setTransactional(boolean value) throws MbException;
+
 }
--- wmb-util/src/main/java/com/googlecode/wmbutil/messages/header/MbPropertiesHeaderImpl.java
+++ wmb-util/src/main/java/com/googlecode/wmbutil/messages/header/MbPropertiesHeaderImpl.java
@@ -3,6 +3,8 @@ package com.googlecode.wmbutil.messages.header;
 import com.ibm.broker.plugin.MbElement;
 import com.ibm.broker.plugin.MbException;
 
+import java.util.Date;
+
 /**
  * @author Bob Browning <bob.browning@pressassociation.com>
  */
@@ -12,43 +14,187 @@ public class MbPropertiesHeaderImpl extends AbstractMbHeader implements MbProper
         super(elm, MbHeaderType.PROPERTIES);
     }
 
+    @Override public int getCodedCharSetId() throws MbException {
+        return this.<Integer>getValue(CodedCharSetId);
+    }
+
+    @Override public Date getCreationTime() throws MbException {
+        return getValue(CreationTime);
+    }
+
+    @Override public String getContentType() throws MbException {
+        return getValue(ContentType);
+    }
+
+    @Override public int getEncoding() throws MbException {
+        return this.<Integer>getValue(Encoding);
+    }
+
+    @Override public Date getExpirationTime() throws MbException {
+        return getValue(ExpirationTime);
+    }
+
+    @Override public String getIdentityMappedIssuedBy() throws MbException {
+        return getValue(IdentityMappedIssuedBy);
+    }
+
+    @Override public String getIdentityMappedPassword() throws MbException {
+        return getValue(IdentityMappedPassword);
+    }
+
+    @Override public String getIdentityMappedToken() throws MbException {
+        return getValue(IdentityMappedToken);
+    }
+
+    @Override public String getIdentityMappedType() throws MbException {
+        return getValue(IdentityMappedType);
+    }
+
+    @Override public String getIdentitySourceIssuedBy() throws MbException {
+        return getValue(IdentitySourceIssuedBy);
+    }
+
+    @Override public String getIdentitySourcePassword() throws MbException {
+        return getValue(IdentitySourcePassword);
+    }
+
+    @Override public String getIdentitySourceToken() throws MbException {
+        return getValue(IdentitySourceToken);
+    }
+
+    @Override public String getIdentitySourceType() throws MbException {
+        return getValue(IdentitySourceType);
+    }
+
     @Override
     public String getMessageFormat() throws MbException {
-        return getValue(FORMAT);
+        return getValue(MessageFormat);
     }
 
     @Override
     public void setMessageFormat(String messageFormat) throws MbException {
-        setValue(FORMAT, messageFormat);
+        setValue(MessageFormat, messageFormat);
     }
 
     @Override
     public String getMessageSet() throws MbException {
-        return getValue(MSG_SET);
+        return getValue(MessageSet);
     }
 
     @Override
     public void setMessageSet(String messageSet) throws MbException {
-        setValue(MSG_SET, messageSet);
+        setValue(MessageSet, messageSet);
     }
 
     @Override
     public String getMessageType() throws MbException {
-        return getValue(MSG_TYPE);
+        return getValue(MessageType);
+    }
+
+    @Override public boolean getPersistence() throws MbException {
+        return this.<Boolean>getValue(Persistence);
+    }
+
+    @Override public int getPriority() throws MbException {
+        return this.<Integer>getValue(Priority);
+    }
+
+    @Override public byte[] getReplyIdentifier() throws MbException {
+        return getValue(ReplyIdentifier);
+    }
+
+    @Override public String getReplyProtocol() throws MbException {
+        return getValue(ReplyProtocol);
     }
 
     @Override
     public void setMessageType(String messageType) throws MbException {
-        setValue(MSG_TYPE, messageType);
+        setValue(MessageType, messageType);
+    }
+
+    @Override public void setPersistence(boolean value) throws MbException {
+        setValue(Persistence, value);
+    }
+
+    @Override public void setPriority(int value) throws MbException {
+        setValue(Priority, value);
+    }
+
+    @Override public void setReplyIdentifier(byte[] value) throws MbException {
+        setValue(ReplyIdentifier, value);
+    }
+
+    @Override public void setReplyProtocol(String value) throws MbException {
+        setValue(ReplyProtocol, value);
     }
 
     @Override
     public String getTopic() throws MbException {
-        return getValue(TOPIC);
+        return getValue(Topic);
+    }
+
+    @Override public boolean getTransactional() throws MbException {
+        return this.<Boolean>getValue(Transactional);
+    }
+
+    @Override public void setCodedCharSetId(int value) throws MbException {
+        setValue(CodedCharSetId, value);
+    }
+
+    @Override public void setCreationTime(Date value) throws MbException {
+        setValue(CreationTime, value);
+    }
+
+    @Override public void setContentType(String value) throws MbException {
+        setValue(ContentType, value);
+    }
+
+    @Override public void setEncoding(int value) throws MbException {
+        setValue(Encoding, value);
+    }
+
+    @Override public void setExpirationTime(Date value) throws MbException {
+        setValue(ExpirationTime, value);
+    }
+
+    @Override public void setIdentityMappedIssuedBy(String value) throws MbException {
+        setValue(IdentityMappedIssuedBy, value);
+    }
+
+    @Override public void setIdentityMappedPassword(String value) throws MbException {
+        setValue(IdentityMappedPassword, value);
+    }
+
+    @Override public void setIdentityMappedToken(String value) throws MbException {
+        setValue(IdentityMappedToken, value);
+    }
+
+    @Override public void setIdentityMappedType(String value) throws MbException {
+        setValue(IdentityMappedType, value);
+    }
+
+    @Override public void setIdentitySourceIssuedBy(String value) throws MbException {
+        setValue(IdentitySourceIssuedBy, value);
+    }
+
+    @Override public void setIdentitySourcePassword(String value) throws MbException {
+        setValue(IdentitySourcePassword, value);
+    }
+
+    @Override public void setIdentitySourceToken(String value) throws MbException {
+        setValue(IdentitySourceToken, value);
+    }
+
+    @Override public void setIdentitySourceType(String value) throws MbException {
+        setValue(IdentitySourceType, value);
     }
 
     @Override
     public void setTopic(String topic) throws MbException {
-        setValue(TOPIC, topic);
+        setValue(Topic, topic);
+    }
+
+    @Override public void setTransactional(boolean value) throws MbException {
+        setValue(Transactional, value);
     }
 }
