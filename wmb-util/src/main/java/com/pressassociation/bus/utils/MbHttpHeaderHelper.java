c216c2c801d5236bf0ecf528d2fb14da6bfe862f
Bob Browning <rpb6@drache.co.uk>
2012-12-03 18:42:28 +0000

    Add http header helper class

--- wmb-util/src/main/java/com/pressassociation/bus/utils/MbHttpHeaderHelper.java
+++ wmb-util/src/main/java/com/pressassociation/bus/utils/MbHttpHeaderHelper.java
@@ -6,11 +6,10 @@ import com.ibm.broker.plugin.MbException;
 /**
  * @author Bob Browning <bob.browning@pressassociation.com>
  */
-@SuppressWarnings("unused")
 public final class MbHttpHeaderHelper {
     private MbHttpHeaderHelper() {}
 
-    private static void configureProxyRequestHeaders(
+    public static void configureProxyRequestHeaders(
             MbHttpHeader inputHeader, MbHttpHeader requestHeader) throws MbException {
         requestHeader.setCustomHeader("X-Forwarded-For", inputHeader.getCustomHeader("X-Remote-Host"));
         requestHeader.setCustomHeader("X-Real-IP", inputHeader.getCustomHeader("X-Remote-Addr"));
