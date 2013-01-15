072539ffa49ee2c92ce547bbc5e8d1dee1eebf3b
Bob Browning <rpb6@drache.co.uk>
2013-01-15 14:01:47 +0000

    Add ability to provide cause when initialising MbHttpException

--- wmb-util/src/main/java/com/pressassociation/bus/MbHttpException.java
+++ wmb-util/src/main/java/com/pressassociation/bus/MbHttpException.java
@@ -7,23 +7,35 @@ import com.ibm.broker.plugin.MbUserException;
 import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("UnusedDeclaration")
 public class MbHttpException extends MbUserException {
 
-    private static final String MESSAGE_CATALOG_BIP2950 = "2950";
+    private static final String MESSAGE_NUMBER = "2950";
+    private static final String MESSAGE_CATALOG_NAME = "BIPmsgs";
 
     private MbHttpException(String className, String methodName, String lineNumber, String messageCatalog, String additionalText, Object[] inserts) {
         super(className, methodName, lineNumber, messageCatalog, additionalText, inserts);
     }
 
+    public static MbHttpException buildException(int statusCode, String message, Throwable cause, Object... arguments) throws MbUserException {
+        final MbHttpException e = createRawException(statusCode, message, arguments);
+        e.initCause(cause);
+        return e;
+    }
+
     public static MbHttpException buildException(int statusCode, String message, Object... arguments) throws MbUserException {
+        final MbHttpException e = createRawException(statusCode, message, arguments);
+        e.fillInStackTrace();
+        return e;
+    }
+
+    private static MbHttpException createRawException(int statusCode, String message, Object... arguments) {
         final StackTraceElement caller = Thread.currentThread().getStackTrace()[1];
         List<Object> inserts = Lists.newArrayList();
         inserts.add(statusCode);
         inserts.add(Preconditions.checkNotNull(message));
         inserts.addAll(Arrays.asList(arguments));
-        MbHttpException e = new MbHttpException(
-                caller.getClassName(), caller.getMethodName(), "", MESSAGE_CATALOG_BIP2950, "", inserts.toArray());
-        e.fillInStackTrace();
-        return e;
+        return new MbHttpException(
+                caller.getClassName(), caller.getMethodName(), MESSAGE_CATALOG_NAME, MESSAGE_NUMBER, "", inserts.toArray());
     }
 }
